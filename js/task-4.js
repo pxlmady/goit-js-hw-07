1bf000fb9315428c9b2777f88412ec72551836b4
pxlmady <144046046+pxlmady@users.noreply.github.com>
2023-12-29 12:21:01 +0200

    task 4 script done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.querySelector('input[name="email"]');
+  const passwordInput = loginForm.querySelector('input[name="password"]');
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
