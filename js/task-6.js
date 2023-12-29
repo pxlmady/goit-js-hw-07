fef24f2041c53bc41504efd06a4ec6c233362c17
pxlmady <144046046+pxlmady@users.noreply.github.com>
2023-12-29 15:54:38 +0200

    task 6 script done

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,42 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+
+  boxesContainer.innerHTML = '';
+
+  if (isNaN(amount) || amount < 1 || amount > 100) {
+    alert('Please enter a number between 1 and 100.');
+    return;
+  }
+
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+}
+
+document.querySelector('button[data-create]').addEventListener('click', function () {
+  const inputNumber = document.querySelector('input[type="number"]');
+  createBoxes(Number(inputNumber.value));
+  inputNumber.value = '';
+});
+
+document.querySelector('button[data-destroy]').addEventListener('click', destroyBoxes);
